8decd556c5fa9517481c6989144bd5713ab46a7b
Tanvir Tanjum Shourav <39719570+tanvirtanjum@users.noreply.github.com>
2020-04-15 17:04:21 +0600

    ~~TANVIR

--- Pages/employeeList.php
+++ Pages/employeeList.php
@@ -9,7 +9,7 @@ include '../Page Controllers/employeeListController.php';
 		<link rel="stylesheet" type="text/css" href="CSS/employeeList.css">
 	</head>
 																						
-	<body style="background-image: url('Images/BGB.jpg'); background-repeat: no-repeat;">
+	<body style="background-image: url('Images/BGB.jpg'); background-repeat: no-repeat; background-size: 100% 100%;">
 		<form action="" method="post">
 			<div class="container">
 				<div class="floatRightTop">
