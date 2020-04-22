acdf7c313033a92105c49a906d92e486fe1643dc
Tanvir Tanjum Shourav <39719570+tanvirtanjum@users.noreply.github.com>
2020-04-22 19:16:49 +0600

    ~TANVIR

--- Pages/salesHistory.php
+++ Pages/salesHistory.php
@@ -1,46 +1,5 @@
 <?php
-if(isset($_COOKIE['uid']) && isset($_COOKIE['sid']))
-{
-	$uid = $_COOKIE['uid'];
-	$sid = $_COOKIE['sid'];
-	
-	
-	$srchProdTF="";
-	$srchProdTFerror="";
-	
-	if(isset($_POST["logoutBTN"]))
-	{
-		setcookie("uid",$uid,time()-600);
-		setcookie("sid",$sid,time()-600);
-		header("Location:index.php");
-	}
-	
-	if(isset($_POST["gohomebtn"]))
-	{
-		setcookie("uid",$uid,time()+600);
-		setcookie("sid",$sid,time()+600);
-		header("Location:home.php");
-	}
-	
-	if(isset($_POST["srchBTN"]))
-	{
-		if(empty($_POST["srchProdTF"]))
-		{
-			$srchProdTFerror="*";
-		}
-		
-		else
-		{
-			$srchProdTF=$_POST["srchProdTF"];
-		}
-	
-	}
-}
-
-else
-{
-	header("Location:index.php");
-}	
+include '../Page Controllers/salesHistoryController.php';
 ?>
 
 <!DOCTYPE html>
