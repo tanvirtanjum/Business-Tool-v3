cdbab0d8660458220e7df3bf6e9a5ff60a747c64
Tanvir Tanjum Shourav <39719570+tanvirtanjum@users.noreply.github.com>
2020-05-06 23:25:30 +0600

    Some CSS Adjusted..~TANVIR

--- Pages/home.php
+++ Pages/home.php
@@ -16,31 +16,31 @@ include '../Page Controllers/homeController.php';
 				<input type="hidden" id="sid" value="<?php echo $_COOKIE['sid']; ?>">
 				<table align="middle" valign="center">
 					<tr>
-						<td> <input type="image" src="Images\employee_list_option.png" alt="employee_list" class="button" name="employeeBTN" onclick="return access()"> </td>
+						<td align="middle"> <input type="image" src="Images\employee_list_option.png" alt="employee_list" class="button" name="employeeBTN" onclick="return access()"> </td>
 						<td class="col"></td>
-						<td> <input type="image" src="Images\product_list_option.png" alt="product_list" class="button" name="productBTN" onclick="return access()"> </td>
+						<td align="middle"> <input type="image" src="Images\product_list_option.png" alt="product_list" class="button" name="productBTN" onclick="return access()"> </td>
 						<td class="col"></td>
-						<td> <input type="image" src="Images\sell_product_opt.png" alt="sell_product" class="button" name="sellBTN"> </td>
+						<td align="middle"> <input type="image" src="Images\sell_product_opt.png" alt="sell_product" class="button" name="sellBTN"> </td>
 						<td class="col"></td>
-						<td> <input type="image" src="Images\note_option.png" alt="note_option" class="button" name="takenoteBTN"> </td>
+						<td align="middle"> <input type="image" src="Images\note_option.png" alt="note_option" class="button" name="takenoteBTN"> </td>
 						<td class="col"></td>
-						<td> <input type="image" src="Images\user_opt.png" alt="about_me" class="button" name="aboutmeBTN"> </td>
+						<td align="middle"> <input type="image" src="Images\user_opt.png" alt="about_me" class="button" name="aboutmeBTN"> </td>
 						<td class="col"></td>
-						<td> <input type="image" src="Images\faq_option.png" alt="learn_more" class="button" name="learnmoreBTN"> </td>
+						<td align="middle"> <input type="image" src="Images\faq_option.png" alt="learn_more" class="button" name="learnmoreBTN"> </td>
 					</tr>	
 					
 					<tr>
-						<td class="text" valign="bottom" align="center"><span>Employee List</span> </td>
+						<td class="text" valign="bottom" align="middle"><span>Manage Employees</span> </td>
 						<td></td>
-						<td class="text" valign="bottom" align="center"><span>Product List</span> </td>
+						<td class="text" valign="bottom" align="middle"><span>Manage Products</span> </td>
 						<td></td>
-						<td class="text" valign="bottom" align="center"><span>Sell Product</span> </td>
+						<td class="text" valign="bottom" align="middle"><span>Sell Products</span> </td>
 						<td></td>
-						<td class="text" valign="bottom" align="center"><span>Take Note</span> </td>
+						<td class="text" valign="bottom" align="middle"><span>Take Notes</span> </td>
 						<td></td>
-						<td class="text" valign="bottom" align="center"><span><?php echo "$uid";  ?></span> </td>
+						<td class="text" valign="bottom" align="middle"><span>About <?php echo "$uid";  ?></span> </td>
 						<td></td>
-						<td class="text" valign="bottom" align="center"><span>Learn More</span> </td>
+						<td class="text" valign="bottom" align="middle"><span>Learn More</span> </td>
 					</tr>
 					
 					<tr>
--- Pages/learnMore.php
+++ Pages/learnMore.php
@@ -65,39 +65,46 @@ include '../Page Controllers/learnMoreController.php';
 					</tr>
 				</table>	
 			
-				<table>
+				<table style="width:100%;">
 					<tr>
 						<td style="height:100px;"></td>
 					</tr>
 					<tr>
-						<td>
-							<span class="insheader"> <br> Instructions to be followed for the system~ </span>	
+						<td align="middle" valign="center">
+							<span class="insheader"> <br> System Manual</span>	
 						</td>
+					</tr>
+					<tr>
+						<td style="height:50px;"></td>
 					</tr>
 				</table>
 				
-				<table style="width:100%;">
+				<table style="width:100%;" cellspacing="0" cellpadding="0">
 					<tr>
 						<td align="middle" style="width: calc(33.333333333333333333333333333333%);"></td>
 						
 						<td align="middle" style="width: calc(33.333333333333333333333333333333%);">
-							<div class="cardTable">
-								<div class="flip-box">
-								  <div class="flip-box-inner">
-									<div class="flip-box-front">
-									<h3> Login </h3>
-									<img src="Images/learnLogin.png" alt="Avatar" style="width:70%;height:80%;">
-									</div>
-									<div class="flip-box-back">
-										<h1 align="middle">LOGIN</h1>
-										<h3> Login- User must login with a valid user-id & password. 
-										<br> The loggedin User can be an ADMIN, a MANAGER or a SALESMAN. 
-										<br> The tasks will be enabled accroding to their designations. 
-										<br> After a succesful login user will get access to HOME-PAGE. 
-										</h3>
-									</div>
-								  </div>
+							<div class="flip-box">
+							  <div class="flip-box-inner">
+								<div class="flip-box-front">
+								<h3> Login </h3>
+								<img src="Images/learnLogin.png" alt="Avatar" style="width:70%;height:80%;">
 								</div>
+								<div class="flip-box-back">
+									<h1 align="middle">LOGIN</h1>
+									<span> 
+										Every user have to login with their Organization ID and PASSWORD to use the syetem. Otherwise the rest pages will not appear just by giving the URL of the page.
+										<br>
+										After validation of the user the sytem will take the user to the home page.
+										<br>
+										The accesibility pages and the task that can be done by the user further will be dependent on the users DESIGNATION.
+										<br>
+										The system will deny to access those pages which are not for that user. 
+										<br> <br>
+										Every user can recover their account password by clicking on the "Forgotten Password?" section by giving valid informations.
+									<span>
+								</div>
+							  </div>
 							</div>
 						</td>
 						
@@ -106,134 +113,267 @@ include '../Page Controllers/learnMoreController.php';
 			
 					<tr>
 						<td align="middle" style="width: calc(33.333333333333333333333333333333%);">
-							<div class="cardTable">
+							
 								<div class="flip-box">
 								  <div class="flip-box-inner">
 									<div class="flip-box-front">
-									<h3> Home-Page </h3> 
+									<h3> HOME </h3> 
 									<img src="Images/learnHome.png" alt="Avatar" style="width:70%;height:80%;">
 									</div>
 									<div class="flip-box-back">
-										<h1> Home-Page contains the following </h1>
-										<h3> Emplyee List </h3>
-										<h3> Product List </h3>
-										<h3> Sell Product </h3>
-										<h3> Take Note </h3>
-										<h3> About Me </h3>
-										<h3> Learn More </h3>
+										<h1 align="middle">HOME</h1>
+										<span> 
+											Every user will be shown this page after successful login.
+											<br>
+											User will see 6 sections excluding logout.
+											<br>
+											Sections:
+											<ul> 
+												<li>MANAGE EMPLOYEES(Not Accessible by SALESMAN.)</li>
+												<li>MANAGE PRODUCTS(Not Accessible by SALESMAN.)</li>
+												<li>SELL PRODUCTS</li>
+												<li>TAKE NOTES</li>
+												<li>ABOUT USER</li>
+												<li>LEARN MORE</li>
+											</ul>
+											<br><br>
+											Users can browse these section according to their privilege.
+											<br>
+											System will autometically deny the request if the user has no accesibility.			
+										</span>
 									</div>
 								  </div>
 								</div>
-							</div>
+							
 						</td>
 					
 						<td align="middle" style="width: calc(33.333333333333333333333333333333%);">
-							<div class="cardTable">
+							
 								<div class="flip-box">
 									<div class="flip-box-inner">
 									<div class="flip-box-front">
-										<h3> Employee-List </h3>
+										<h3> MANAGE EMPLOYEES </h3>
 										<img src="Images/learnEmployeeList.png" alt="Avatar" style="width:70%;height:80%;">										
 									</div>
 									<div class="flip-box-back">
-											<h1>Employees Informations</h1>
-											<h3>Search: Accessible for all user.</h3>
-											<h3>Insert: Accessible for ADMIN only. </h3>
-											<u> <h3>~ For every insertion, a random password will be generated for that inserted new user for future login. </h3 > </u>
-											<h3>Update: Accessible for ADMIN & MANAGER.</h3>
-											<h3>Delete: Accessible for ADMIN only. </h3>
-											<h3>Refresh: Accessible for all user.</h3>
+										<h1 align="middle">MANAGE EMPLOYEES</h1>
+										<span> 
+											Only ADMINS and MANAGERS can browse this page.
+											<br>
+											This page is all about employee management.
+											<br>
+											Tasks, that can be done on this page-
+											<ul> 
+												<li>
+													Search: Viewing information of a particular employee.
+												</li> 
+												
+												<li>
+													Advance Searching: Viewing all employees information by sorting list according some insticts.
+													It will take the user in a new window.
+													<br> Also can print the sorted list.
+												</li> 
+												
+												<li>
+													Insert: Assigning New Employees(Only by ADMIN.).
+													<br>On each successful inserting a random password will be given to that inserted user by which the assigned user will bw able to login tothe system.
+												</li>
+												
+												<li>
+													Update: Updating Employees information(Only by ADMIN.).
+												</li>
+												<li>
+													Delete: De-activating Employees(Only by ADMIN.).
+													<br>That deleted user will not be able to login until re-activation.
+												</li>
+												<li>Print: It will print the whole active employees list.</li>
+											</ul>
+										</span>
 									</div>
 								  </div>
 								</div>
-							</div>
+							
 						</td>
 					
 						<td align="middle" style="width: calc(33.333333333333333333333333333333%);">
-							<div class="cardTable">
-								<div class="flip-box">
-									<div class="flip-box-inner">
-										<div class="flip-box-front">
-											<h3> Product-List </h3> 
-											<img src="Images/learnProductList.png" alt="Avatar" style="width:70%;height:80%;">
-										</div>
-									<div class="flip-box-back">
-							  
-										<h1>Product Information</h1>
-										<h3>Search: Accessible for all user.</h3>
-										<h3>Insert: Accessible for ADMIN & MANAGER. </h3>
-										<h3>Update: Accessible for ADMIN & MANAGER.</h3>
-										<h3>Delete: Accessible for ADMIN only.</h3>
-										<h3>Refresh: Accessible for all user.</h3>
+							<div class="flip-box">
+								<div class="flip-box-inner">
+									<div class="flip-box-front">
+										<h3> MANAGE PRODUCTS </h3> 
+										<img src="Images/learnProductList.png" alt="Avatar" style="width:70%;height:80%;">
 									</div>
-								  </div>
+								<div class="flip-box-back">
+									<h1 align="middle">MANAGE PRODUCTS</h1>
+									<span> 
+										Only ADMINS and MANAGERS can browse this page.
+										<br>
+										This page is all about product management.
+										<br>
+										Tasks, that can be done on this page-
+										<ul> 
+											<li>
+												Search: Viewing information of a particular product.
+											</li> 
+											
+											<li>
+												Advance Searching: Viewing all products information by sorting list according some insticts.
+												It will take the user in a new window.
+												<br> Also can print the sorted list.
+											</li> 
+											
+											<li>
+												Insert: Adding New Products.
+											</li>
+											
+											<li>
+												Update: Updating Products information.
+											</li>
+											<li>
+												Delete: Making products unavailable for sale(Only by ADMIN.).
+											</li>
+											<li>Print: It will print the whole available products list.</li>
+										</ul>
+									</span>
 								</div>
+							  </div>
 							</div>
 						</td>
 					</tr>		
 					
 					<tr>
 						<td align="middle" style="width: calc(33.333333333333333333333333333333%);">
-							<div class="cardTable">
-								<div class="flip-box">
-									<div class="flip-box-inner">
-										<div class="flip-box-front">
-											<h3> Sell-Product </h3> 
-											<img src="Images/learnSellProduct.png" alt="Avatar" style="width:70%;height:80%;">
-										</div>
-									<div class="flip-box-back">
-									  
-										<h1>Product Sales</h1>
-										<h3>Search: Accessible for all user.</h3>
-										<h3>Sell: Accessible for SALESMAN. </h3>
-										<h3>Discard: Accessible for SALESMAN.</h3>
-										<u> <h2> Sales History </h2> </u>
-										<h3>Search: Accessible for all user. </h3>
-										<h3>Refresh: Accessible for all user.</h3>
+							<div class="flip-box">
+								<div class="flip-box-inner">
+									<div class="flip-box-front">
+										<h3> SELL PRODUCTS </h3> 
+										<img src="Images/learnProductList.png" alt="Avatar" style="width:70%;height:80%;">
 									</div>
-								  </div>
+								<div class="flip-box-back">
+									<h1 align="middle">SELL PRODUCTS</h1>
+									<span> 
+										All users can browse this page.
+										<br>
+										This page is all about sales.
+										<br>
+										Tasks, that can be done on this page-
+										<ul> 
+											<li>
+												Advance Searching: Viewing all products information by sorting list according name or/and type of products.
+											</li> 
+											
+											<li>
+												Sell: Selling Products(Only by SALESMAN).
+												<br>On successful sale "Print Invoice" will be activated.
+												<br>On each sale, records will be stored.
+											</li>
+											
+											<li>
+												Print Invoice: Printing the invoice for the user.
+											</li>
+											<li>
+												Sales History: See the sales records.
+												<br>It will take the user in a new page.
+												<br>User can see the whole record list and also also search for a particular one.
+												<br>The list can be sorted by selling date or/and customer name.
+												<br>User also can take print of the whole or sorted table.
+											</li>
+										</ul>
+									</span>
 								</div>
+							  </div>
 							</div>
 						</td>
 					
 						<td align="middle" style="width: calc(33.333333333333333333333333333333%);">
-							<div class="cardTable">
+						
 								<div class="flip-box">
 									<div class="flip-box-inner">
 										<div class="flip-box-front">
-											<h3> Take-Note </h3> 
+											<h3> TAKE NOTES </h3> 
 											<img src="Images/learnTakeNotes.png" alt="Avatar" style="width:70%;height:80%;">
 										</div>
 									<div class="flip-box-back">
-							  
-										<h1>Notes Saving Cloud</h1>
-										<h3>Download: Accessible for all user.</h3>
-										<h3>Upload: Accessible for all user. </h3>
-										<h3>Refresh: Accessible for all user.</h3>
-										<h3>Update: Accessible for all user.</h3>
-										<h3>Delete: Accessible for all user.</h3>
-										<h3>Print: Accessible for all user.</h3>
+										<h1 align="middle">TAKE NOTES</h1>
+										<span> 
+											All users can browse this page.
+											<br>
+											This page is all about taking personal notes.
+											<br>
+											Tasks, that can be done on this page-
+											<ul>
+												<li>
+													Each user only can view their own notes list.
+												</li>
+												<li>
+													Upload: Writing notes and save it in the system.
+												</li> 
+												
+												<li>
+													Download: Downloading the saved notes by the user.(Not other users note)
+												</li>
+												
+												<li>
+													Print: Save the downloaded note on PC in form of text file.
+												</li>
+												
+												<li>
+													Update: Updating saved notes by the user.
+												</li>
+												
+												<li>
+													Update: Deleting saved notes by the user.
+												</li>
+											</ul>
+										</span>
 									</div>
 								  </div>
-								</div>
 							</div>
 						</td>
 					
 						<td align="middle" style="width: calc(33.333333333333333333333333333333%);">
-							<div class="cardTable">
-								<div class="flip-box">
-									<div class="flip-box-inner">
-										<div class="flip-box-front">
-											<h3> About-me </h3> 
-											<img src="Images/learnAboutMe.png" alt="Avatar" style="width:70%;height:80%;">
-										</div>
-									<div class="flip-box-back">
-							  
-										<h1> Logged-In user information.</h1>
-										<h3> Settings: Accessible for all user</h3>
+							<div class="flip-box">
+								<div class="flip-box-inner">
+									<div class="flip-box-front">
+										<h3> ABOUT USER </h3> 
+										<img src="Images/learnAboutMe.png" alt="Avatar" style="width:70%;height:80%;">
 									</div>
-								  </div>
+								<div class="flip-box-back">
+									<h1 align="middle">ABOUT USER</h1>
+									<span> 
+										All users can browse this page.
+										<br>
+										This page is all about the logged in users personal information.
+										<br>
+										Tasks, that can be done on this page-
+										<ul>
+											<li>
+												Settings: For changing password
+											</li>
+											<li>
+												Messages.
+												<ul>
+													<li>
+														User can view the list of seen and unseen inboxs(SEPARATED LIST).
+														<br>User can also view his/her sent messages list.
+													</li>
+													<li>
+														Send Message: User can send message to other valid users.
+														<br>Message can contain attachments like DOCCUMENTS, IMAGE, SOFTWARES etc.
+														<br>N.B. Attachments size should not be more tha 10MB.
+													</li>
+													<li>
+														Read Inbox: User can read the messages that are sent for the user by other users.
+														<br>Also the user can download if there is any attachment.
+													</li>
+													<li>
+														Read Sent Messages: User can read the messages that the user sent other users previously..
+													</li>
+												</ul>
+											</li> 
+										</ul>
+									</span>
 								</div>
+							  </div>
 							</div>
 						</td>
 					</tr>		
